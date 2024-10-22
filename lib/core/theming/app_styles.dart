80ad833efdb7b2835d75b26b6ff0dabfe15fb23f
Nadia khaled <d703403@gmail.com>
2024-10-22 16:08:37 +0300

    final6 commit

--- lib/core/theming/app_styles.dart
+++ lib/core/theming/app_styles.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:untitled/core/theming/app_colors.dart';
 
