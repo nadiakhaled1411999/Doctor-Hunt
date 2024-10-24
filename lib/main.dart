95bbcbd9a9ffa82b62b859a3d41d57abc27500a5
Nadia khaled <d703403@gmail.com>
2024-10-24 08:18:01 +0300

    final8 commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:untitled/doctor_hunt_app.dart';
-import 'package:untitled/features/onboarding/presentation/screens/onboarding.dart';
+ 
  
 
 void main()async {
