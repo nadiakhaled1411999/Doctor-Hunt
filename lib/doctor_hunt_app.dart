67ddf9af764a28fda82fce3ef8ae1777ca1a5dcb
Nadia khaled <d703403@gmail.com>
2024-11-15 19:33:34 +0200

    Done_Task4

--- lib/doctor_hunt_app.dart
+++ lib/doctor_hunt_app.dart
@@ -1,40 +1,49 @@
-import 'package:flutter/material.dart';
+  
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:untitled/core/constants/app_strings.dart';
-
+import 'package:untitled/core/routing/route_export.dart';
 import 'core/routing/app_router.dart';
-import 'features/onboarding/presentation/screens/onboarding.dart';
+
 
 class DoctorHuntApp extends StatelessWidget {
-  
   const DoctorHuntApp({super.key});
- 
+
+  
+  Future<bool> _isLoggedIn() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token') != null;
+  }
+
   @override
-  Widget build(BuildContext context){
-    
-    final DoctorHuntRouter  router =DoctorHuntRouter();
-    return   ScreenUtilInit(
+  Widget build(BuildContext context) {
+    final DoctorHuntRouter router = DoctorHuntRouter();
+    return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      
-      builder: (_ , child) {
-        return MaterialApp(
-          builder: EasyLoading.init(),
-          debugShowCheckedModeBanner: false,
-          title:  AppStrings.appName,
-
-          home:OnboardingPage(),
-          onGenerateRoute: router.generateRoute,
-
-
-
-
+      builder: (_, child) {
+        return FutureBuilder<bool>(
+          future: _isLoggedIn(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return MaterialApp(
+                builder: EasyLoading.init(),
+                debugShowCheckedModeBanner: false,
+                title: AppStrings.appName,
+                
+                
+                home: snapshot.data == true ? HomeScreen() : OnboardingPage(),
+                
+                onGenerateRoute: router.generateRoute,
+              );
+            }
+          },
         );
       },
-
     );
   }
 }
-
